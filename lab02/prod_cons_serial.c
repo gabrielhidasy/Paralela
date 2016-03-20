4f485b478c06ac2eac77bb97fd82e3b79b307681
Gabriel Hidasy <gabrielhidasy@gmail.com>
2016-03-19 23:01:31 -0300

    Add initial files for laboratory 2

---
+++ lab02/prod_cons_serial.c
@@ -0,0 +1,19 @@
+void producer_consumer(int *buffer, int size, int *vec, int n) {
+	int i, j;
+	long long unsigned int sum = 0;
+
+	for(i=0;i<n;i++) {
+		if(i % 2 == 0) {	// PRODUTOR
+			for(j=0;j<size;j++) {
+				buffer[j] = vec[i] + j*vec[i+1];
+			}
+		}
+		else {	// CONSUMIDOR
+			for(j=0;j<size;j++) {
+				sum += buffer[j];
+			}
+		}
+	}
+	printf("%llu\n",sum);
+}
+
